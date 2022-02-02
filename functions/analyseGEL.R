e321f91b6421c2d9954cb37f1747435255842deb
Shaima Belhechmi <60618081+ShaimaBelhechmi@users.noreply.github.com>
2022-02-02 17:38:34 +0100

    Update analyseGEL.R

--- functions/analyseGEL.R
+++ functions/analyseGEL.R
@@ -1,5 +1,5 @@
-### Working directory
-setwd(" ")
+### Working directory ###
+#setwd(" ")
 
 #####################################################################
 # R packages installation                                           #
@@ -7,18 +7,19 @@ setwd(" ")
 library(parallel)
 library(grpreg)
 
-set.seed( )
+### Set a seed ###
+#set.seed(...)
 
-#biomarker groups
+### Biomarker groups ###
 #group <- c(rep(1:9, each = 25), rep(10:20, each = 75))
 
-# Number of PC-cores used
+### Number of PC-cores used ###
 pCores <- #80
 
-# Definition of the cluster via 'makeCluster'
+### Definition of the cluster via 'makeCluster' ###
 cl <- makeCluster(pCores, outfile = "OUTcv.grpregGEL.txt")
 
-# Number of iterations
+### Number of iterations ###
 r <- 500
 
 v <-
@@ -44,7 +45,7 @@ analyse <- function(character) {
   }
 }
 
-### Execution of the program
+### Execution of the program ###
 start <- Sys.time()
 z <- clusterApplyLB(cl = cl, v, analyse)
 end <- Sys.time()
